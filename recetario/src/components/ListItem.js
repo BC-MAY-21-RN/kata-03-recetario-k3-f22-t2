9c7dc05504882a04a022031110d7321f1ff00cf0
Jesus <chuchober@hotmail.com>
2022-02-28 17:47:42 -0500

    Add icons to modal
    Co-authored-by: Kelly <kejoh07@gmail.com>
    Co-authored-by: Cris <crprmahdz@yahoo.com>
    Co-authored-by: Vanina <vaniwinnik@gmail.com>

--- recetario/src/components/ListItem.js
+++ recetario/src/components/ListItem.js
@@ -1,60 +1,95 @@
-import { StyleSheet, Text, View, Image, Pressable, Alert, Modal } from 'react-native'
-import React, {useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  Alert,
+  Modal,
+} from 'react-native';
+import React, {useState} from 'react';
 import RecipeDetails from './RecipeDetails';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const ListItem = ( {recipe}) => {   
-    const [modalVisible, setModalVisible] = useState(false);  
-    return (
-        <View style={styles.container}>
-            <Pressable onPress={() => setModalVisible(true)} >
-                <Image style={[recipe.category == 'Trending' ? styles.imgTreding : styles.imgRecent, {borderRadius: 10}]}  source={{ uri: recipe.image}}/>
+const ListItem = ({recipe}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => setModalVisible(true)}>
+        <Image
+          style={[
+            recipe.category == 'Trending'
+              ? styles.imgTreding
+              : styles.imgRecent,
+            {borderRadius: 10},
+          ]}
+          source={{uri: recipe.image}}
+        />
+      </Pressable>
+      <Text style={styles.title}>{recipe.name}</Text>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <RecipeDetails recipe={recipe} />
+        <View style={styles.containerIcons}>
+          <View style={styles.icons}>
+            <Pressable
+              style={styles.close}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Icon name="close" size={30} color="#ffffff" />
             </Pressable>
-            <Text style={styles.title}>{recipe.name}</Text>
-
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                Alert.alert('Modal has been closed.');
-                setModalVisible(!modalVisible);
-                }}>
-                <RecipeDetails recipe={recipe} />
-                <Pressable
-                style={styles.close}
-                onPress={() => setModalVisible(!modalVisible)}>
-                <Icon name="close" size={30} color="#ffffff" />
-                </Pressable>
-            </Modal>
+            <View style={styles.iconsRight}>
+              <Icon name="upload" size={30} color="#ffffff" />
+              <Icon
+                style={styles.heart}
+                name="heart-o"
+                size={30}
+                color="#ffffff"
+              />
+            </View>
+          </View>
         </View>
-    )
-}
+      </Modal>
+    </View>
+  );
+};
 
-export default ListItem
+export default ListItem;
 
 const styles = StyleSheet.create({
-    title: {
-        color: 'white',
-        marginTop: 5,
-        fontSize: 15
-    },
-    imgTreding:{
-        width:   115 , 
-        height:  115 ,
-    },
-    imgRecent: {
-        width: 150,
-        height: 150,
-    },
-    container: {
-        paddingHorizontal: 10,
-        paddingBottom: 15
-        
-    },
-    close: {
-        position: 'absolute',
-        left: 15,
-        top: 15,
-    },
+  title: {
+    color: 'white',
+    marginTop: 5,
+    fontSize: 15,
+  },
+  imgTreding: {
+    width: 115,
+    height: 115,
+  },
+  imgRecent: {
+    width: 150,
+    height: 150,
+  },
+  container: {
+    paddingHorizontal: 10,
+    paddingBottom: 15,
+  },
+  containerIcons: {
+    position: 'absolute',
+    width: '90%',
+    left: 15,
+    top: 15,
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  iconsRight: {flexDirection: 'row'},
+  heart: {marginLeft: 15},
 });
